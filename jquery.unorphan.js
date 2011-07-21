2bc25ffc001bce12ae811ce453321b87a7bf733e
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-21 01:11:52 -0700

    Fixed to account for contiguous blocks of spaces.

--- jquery.unorphan.js
+++ jquery.unorphan.js
@@ -5,7 +5,7 @@ $.fn.unorphan = function() {
     if ((last) && (last.nodeType == 3)) {
       var text     = last.nodeValue;
       var stripped = text.replace(/^\s*|\s*$/g, ' ');
-      var spaces   = stripped.match(/ /g).length;
+      var spaces   = stripped.match(/\s+/g).length;
 
       if (spaces > 0) {
         last.nodeValue = last.nodeValue.replace(/\s*([^\s]+\s*)$/g, '\xA0$1');
